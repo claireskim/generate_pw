a82044024ec0822fb47f2e6bec840362189e59e3
Claire <49087389+claireskim@users.noreply.github.com>
2019-07-03 12:33:37 -0700

    Update pw_gen.py

--- pw_gen.py
+++ pw_gen.py
@@ -1,8 +1,8 @@
 import random
 
-pool = "abcdefghijklmnopqrstuvwxyz1234567890!?"
+pool = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!?"
 passlen = 9
 
-gen_pw = F"Your password is {random.sample(pool,passlen)}."
+gen_pw = F"Your password is {''.join(random.sample(pool,passlen))}"
 
-print gen_pw
+print(gen_pw)
